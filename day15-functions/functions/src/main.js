7b67702620541c96a66faedcaca1b1bc0c38279f
Akash-Nagre <er.akashnagre@gmail.com>
2025-08-26 18:22:42 +0530

    error for empty value

--- day15-functions/functions/src/main.js
+++ day15-functions/functions/src/main.js
@@ -4,7 +4,6 @@
 
 var num1 = document.querySelector("#num1")
 var num2 = document.querySelector("#num2")
-
 var answer = document.querySelector("#answer")
 
 var btn1 = document.getElementById("add")
@@ -12,9 +11,20 @@ var btn2 = document.getElementById("sub")
 var btn3 = document.getElementById("mul")
 var btn4 = document.getElementById("div")
 
+var err = document.getElementById("error")
+
+function checkEmptyValue(){
+  if(num1.value == "" || num2.value == ""){
+    err.textContent = "Error - Values cannot be empty"
+    return false
+  }
+    err.textContent = "‎ "
+  return true
+}
 
 // addEventListener --> listens to an event and executes the function
 btn1.addEventListener("click",function(){
+  if(checkEmptyValue() == false) return
   var n1 = Number(num1.value)
   var n2 = Number(num2.value)
 
@@ -23,4 +33,40 @@ btn1.addEventListener("click",function(){
   // innerHtml
   // textContent
   answer.textContent = "Your addition is : " + (n1 + n2) 
+})
+
+btn2.addEventListener("click",function(){
+  if(checkEmptyValue() == false) return
+  var n1 = Number(num1.value)
+  var n2 = Number(num2.value)
+
+  console.log(n1 - n2)
+
+  // innerHtml
+  // textContent
+  answer.textContent = "Your Subtraction is : " + (n1 - n2) 
+})
+
+btn3.addEventListener("click",function(){
+  if(checkEmptyValue() == false) return
+  var n1 = Number(num1.value)
+  var n2 = Number(num2.value)
+
+  console.log(n1 * n2)
+
+  // innerHtml
+  // textContent
+  answer.textContent = "Your Multiplication is : " + (n1 * n2) 
+})
+
+btn4.addEventListener("click",function(){
+  if(checkEmptyValue() == false) return
+  var n1 = Number(num1.value)
+  var n2 = Number(num2.value)
+
+  console.log(n1 / n2)
+
+  // innerHtml
+  // textContent
+  answer.textContent = "Your Division is : " + (n1 / n2) 
 })
