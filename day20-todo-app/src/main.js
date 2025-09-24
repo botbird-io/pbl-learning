770feb9b39514f908c5b22d768b207f85d29dd07
sanket-sapate <sanketsapatevnit@gmail.com>
2025-09-24 21:13:06 +0530

    conditional classes rendering, delete row function

--- day20-todo-app/src/main.js
+++ day20-todo-app/src/main.js
@@ -1,27 +1,54 @@
-var arr = []
+var arr = [{
+  priority : "High",
+  task_name : "Study"
+},{
+  priority : "Low",
+  task_name : "Sleep"
+},{
+  priority : "High",
+  task_name : "Study"
+},{
+  priority : "Low",
+  task_name : "Sleep"
+}]
 
 var tbody = document.querySelector("tbody")
 
 function displayRows(tasks){
   tbody.innerHTML = null
 
-  tasks.forEach(function(elem,index){
+  tasks.forEach(function(task,index){
     var tr = document.createElement("tr")
     var td1 = document.createElement("td")
     var td2 = document.createElement("td")
     var td3 = document.createElement("td")
 
-    td1.innerText = elem.task_name
-    td2.innerText = elem.priority
-    td3.innerText = "Delete"
+    var span = document.createElement("span")
+    span.innerText = "Delete"
+    span.className = "hover:cursor-pointer"
+    td1.innerText = task.task_name
+    td2.innerText = task.priority
+    td3.append(span)
 
     var tdClasses = "text-center"    
     td1.className = tdClasses
-    td2.className = tdClasses
+    td2.className = tdClasses + " text-white"
     td3.className = tdClasses
+
+    if(task.priority == "High"){
+      td2.className = td2.className + " bg-error"
+    }else if(task.priority == "Low"){
+      td2.className += " bg-success"
+    }
 
     tr.append(td1,td2,td3)
     tbody.append(tr)
+
+
+    // delete event on button
+    span.addEventListener("click",function(){
+      deleteRow(index)
+    })
   })
 
 }
@@ -44,3 +71,8 @@ document.querySelector('form').addEventListener("submit",function(e){
 
   displayRows(arr)
 })
+
+function deleteRow(index){
+  arr.splice(index,1)
+  displayRows(arr)
+}
